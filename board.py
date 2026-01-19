f45c0ec3f1b8b16aea5ab726e210caa22c122974
Vo Phi Hoang <kilogam145236@gmail.com>
2026-01-19 21:39:43 +0700

    [create] generate king queen bishop rook

--- board.py
+++ board.py
@@ -126,6 +126,164 @@ class Board:
                 elif not is_white_knight and self.is_white(target):
                     moves.append((r,c,nr,nc))
         return moves
+    # hàm sinh nước đi cho quân tượng
+    def generate_bishop_moves(self,r,c):
+        # khởi tạo các nước quân tượng có thể đi là hàm rỗng
+        moves =[]
+        # lấy vị trị hiện tại cua quân tượng
+        piece=self.board[r][c]
+        # không phải quân tượng thì không làm gì hết
+        if piece not in ('B','b'):
+            return moves
+        #gán biến phân biệt là quân đen hay trang bây giờ đang là trăng sẽ trả về true
+        is_white_bishop =piece=="B"
+        # 4 hướng đi của quân tượng
+        directions = [
+            (-1,-1),# trái lên
+            (-1,1),# lên phải
+            (1,-1),# trái xuống
+            (1,1)# phải xuống
+        ]
+        # duyệt từng hướng giong luật quân mã nhưng khác là duyệt while thai vì if
+        for dr,dc in directions:
+            nr = r+dr
+            nc = c+dc
+            # duyet nằm trong vi đi từng nước một
+            while 0<=nr<8 and 0<=nc<8:
+                target = self.board[nr][nc]
+                # nếu là dấu chấm trên ma trận thì được đi
+                if target == EMPTY:
+                    moves.append((r,c,nr,nc))
+                # gặp quân đich ăn -> dừng
+                elif is_white_bishop and self.is_black(target):
+                    moves.append((r,c,nr,nc))
+                    break
+                elif not is_white_bishop and  self.is_white(target):
+                    moves.append((r,c,nr,nc))
+                    break
+                # gặp quân cùng màu dừng
+                else :
+                    break
+                # thực hiện cộng để đi xa hơn + lặp lại đến diều kiện dừng
+                nr += dr
+                nc += dc
+        return  moves
+    def generate_rook_moves(self,r,c):
+        # khởi tạo mảng di chuyển quân xe hiện tại là rỗng
+        moves =[]
+        # lấy vị trí hiện tại của quân xe
+        piece = self.board[r][c]
+        # không phải quân xe thì không làm gì
+        if piece not in ('R','r'):
+            return moves
+        # gán biến phân biệt xe trắng và xe đen
+        is_white_rook=piece=="R"
+        # 4 hướng đi của quân xe
+        directions =[
+            (-1,0),#lên
+            (1,0),#xuống
+            (0,-1),# trái
+            (0,1)# phải
+        ]
+        # duyệt các nước đi
+        for dr,dc in directions:
+            nr = r + dr
+            nc= c +dc
+            # đi tung hướng cho đến khi bị chặn
+            while 0<=nr<8 and 0<=nc<8:
+                target = self.board[nr][nc]
+                #kiểm tra có phải rỗng nếu không rong thì được đi
+                if target==EMPTY:
+                    moves.append((r,c,nr,nc))
+                # kiểm tra ăn quân va dung lại
+                elif is_white_rook and self.is_black(target):
+                    moves.append((r,c,nr,nc))
+                    break
+                elif not is_white_rook and self.is_white(target):
+                    moves.append((r,c,nr,nc))
+                    break
+                # ngược nếu trùng màu thì dừng lại
+                else:
+                    break
+                nr += dr
+                nc += dc
+        return moves
+    # sinh nuoc di cho quan hau
+    def generate_queen_moves(self,r,c):
+        # khởi tạo moves bằng rỗng
+        moves = []
+        # lấy vị trí của quân cờ hiện tại
+        piece=self.board[r][c]
+        # kiểm tra có phải quân hậu nếu không phải thì không làm gì hêt
+        if piece not in ('Q','q'):
+            return moves
+        # gán biến để phân biết quân đen và trắng
+        is_white_queen=piece == "Q"
+        # hướng đi của quân hậu là kết hợp của quân tượng(bishop) quân xe(rook)
+        directions = [
+            (-1,0),(1,0),(0,1),(0,-1),
+            (-1,-1),(-1,1),(1,-1),(1,1)
+        ]
+        # duyệt các quân cờ có nước đi phù hợp
+        for dr,dc in directions:
+            nr = r+dr
+            nc = c+dc
+            # duyệt các nước đi đúng hướng cho đến khi bị chan nhu an quân di chuyen dưng rồi lặp lại
+            while 0<=nr<8 and 0<=nc<8:
+                target = self.board[nr][nc]
+                # kiểm tra có rỗng không nếu rỗng được di chuyen
+                if target==EMPTY:
+                    moves.append((r,c,nr,nc))
+                # kiểm tra ăn quân và dừng lai
+                elif is_white_queen and self.is_black(target):
+                    moves.append((r,c,nr,nc))
+                    break
+                elif not is_white_queen and self.is_white(target):
+                    moves.append((r,c,nr,nc))
+                    break
+                # nếu quân cùng màu thì dừng lại
+                else:
+                    break
+                nr += dr
+                nc += dc
+        return moves
+    # hàm di chuyển của quân vua
+    def generate_king_moves(self,r,c):
+        # khởi tạo một mảng di chuyển quân rỗng
+        moves = []
+        # lấy ví trí hiện tại của quân
+        piece = self.board[r][c]
+        # kiểm tra nó có phải quân vua không
+        if piece not in ('K','k'):
+            return moves
+        # gán biến phân biệt quân đen trắng
+        is_white_king= piece == 'K'
+        # 8 hướng đi của quân vua
+        directions = [
+            (-1,1),(1,0),(-1,1),
+            (0,-1),(0,1),
+            (1,-1),(1,0),(1,1)
+        ]
+        # duyệt quân các hướng đi hợp lý của quân vua
+        for dr,dc in directions:
+            nr = r + dr
+            nc = c + dc
+            # kiểm tra ở trong phạm vi
+            if  0<=nr<8 and 0<=nc<8:
+                target = self.board[nr][nc]
+                # kiểm tra di chuyển hợp lệ
+                if target == EMPTY:
+                    moves.append((r,c,nr,nc))
+                # kiểm tra ăn quân
+                elif is_white_king and self.is_black(target):
+                    moves.append((r,c,nc,nr))
+                # ngược lại
+                elif not is_white_king and self.is_white(target):
+                    moves.append((r,c,nr,nc))
+        return moves
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -15,8 +15,32 @@ if __name__ == "__main__":
     knight_move = game.generate_knight_moves(7,6)
     print(knight_move)
     if knight_move:
-        game.make_move(knight_move[1])
+        game.make_move(knight_move[0])
     game.print_board()
-
+    bishop_move = game.generate_bishop_moves(7,5)
+    print(bishop_move)
+    if bishop_move:
+        game.make_move(bishop_move[4])
+    game.print_board()
+    pawn_move = game.generate_pawn_moves(6, 7)
+    print(pawn_move)
+    if pawn_move:
+        game.make_move(pawn_move[1])
+    game.print_board()
+    rook_move = game.generate_rook_moves(7, 7)
+    print(rook_move)
+    if rook_move:
+        game.make_move(rook_move[1])
+    game.print_board()
+    queen_move = game.generate_queen_moves(7, 3)
+    print(queen_move)
+    if queen_move:
+        game.make_move(queen_move[0])
+    game.print_board()
+    king_move = game.generate_king_moves(7, 4)
+    print(king_move)
+    if king_move:
+        game.make_move(king_move[0])
+    game.print_board()
 
 
