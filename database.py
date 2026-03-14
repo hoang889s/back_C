b87e00c84c52dafb6d92e7c307badb2f0f1a787c
Vo Phi Hoang <kilogam145236@gmail.com>
2026-03-14 18:30:48 +0700

    [add] database.py model.py config database and add table user,gamehistory

---
+++ database.py
@@ -0,0 +1,48 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.pool import NullPool
+import os
+# Cấu hình sqlserver 
+# tên server hoặc ip
+DB_SERVER = os.getenv("DB_SERVER","localhost")
+# tên database
+DB_NAME = os.getenv("DB_NAME","chess_online")
+# sa
+DB_USER = os.getenv("DB_USER","sa")
+# mật khẩu
+DB_PASSWORD = os.getenv("DB_PASSWORD","YourPassword123!")
+DB_DRIVER = os.getenv("DB_DRIVER","ODBC Driver 17 for SQL Server")
+# kết nối với server
+# c1
+DATABASE_URL = (
+    f"mssql+pyodbc://{DB_USER}:{DB_PASSWORD}@{DB_SERVER}/{DB_NAME}"
+    f"?driver={DB_DRIVER.replace(' ', '+')}"
+)
+# -- Cách 2: Windows Authentication (uncomment nếu dùng SSMS với Windows Auth)
+# DATABASE_URL = (
+#     f"mssql+pyodbc://@{DB_SERVER}/{DB_NAME}"
+#     f"?driver={DB_DRIVER.replace(' ', '+')}&trusted_connection=yes"
+# )
+engine = create_engine(
+    DATABASE_URL,
+    # tránh lỗi timeout trên SQL server
+    poolclass = NullPool,
+    # đặt true để xem sql query trong console
+    echo = False,
+    fast_executemany=True
+)
+SessionLocal=  sessionmaker(bind=engine,autocommit=False,autoflush=False )
+class Base(DeclarativeBase):
+    pass
+# Helper: lấy session (dùng trong route)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+def init_db():
+    # import để Base biết về các model
+    from models import User, GameHistory   
+    Base.metadata.create_all(bind=engine)
+    print("[DB] Đã khởi tạo schema thành công.")
---
+++ models.py
@@ -0,0 +1,47 @@
+from sqlalchemy import Nullable
+from sqlalchemy import (
+    Column, Integer, String, DateTime, Text,
+    ForeignKey, Enum as SAEnum
+)
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+import enum
+from database import Base
+# trạng thái ván đầu
+class GameResult(str,enum.Enum):
+    WIN = "win"
+    LOSS = "loss"
+    DRAW = "draw"
+    ONGOING = "ongoing"
+# bảng user
+class User(Base):
+    __tablename__ ="users"
+    id = Column(Integer,primary_key=True,index= True)
+    username = Column(String(50),unique=True,nullable=False,index=True)
+    email = Column(String(120),unique=True,nullable=False,index=True)
+    password_hasd = Column(String(255),nullable=False)
+    created_at = Column(DateTime(timezone=True),server_default=func.now())
+    games_as_white = relationship(
+        "GameHistory",foreign_keys = "GameHistory.white_player_id",back_populates = "white_player"
+    )
+    games_as_black = relationship(
+        "GameHistory",foreign_keys = "GameHistory.black_player_id",back_populates = "black_player" 
+    )
+    def __repr__(self):
+        return f"<User id={self.id} username={self.username}>"
+# bảng GameHistory lưu kết quả mỗi ván đấu
+class GameHistory(Base):
+    __tablename__ = "game_history"
+    id = Column(Integer,primary_key = True,index = True)
+    # null = AI
+    white_player_id = Column(Integer,ForeignKey("users.id"),nullable=True)
+    black_player_id = Column(Integer,ForeignKey("users.id"),nullable=True)
+    result = Column(SAEnum(GameResult),default = GameResult.ONGOING,nullable=False)
+    pgn_moves = Column(Text,nullable=True)
+    total_moves = Column(Integer,default=0)
+    started_at = Column(DateTime(timezone=True),server_default=func.now())
+    ended_at = Column(DateTime(timezone=True), nullable=True)
+    white_player = relationship("User",foreign_keys = [white_player_id],back_populates = "games_as_white")
+    black_player = relationship("User",foreign_keys = [black_player_id],back_populates = "games_as_black")
+    def __repr__(self):
+        return f"<GameHistory id ={self.id} result ={self.result}>"
